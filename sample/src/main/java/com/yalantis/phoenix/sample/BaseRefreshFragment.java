6895e239e5cd4de0de3820493cdc93b2311d3f34
dmorozov <dmytro.morozov@yalantis.net>
2022-09-20 13:06:39 +0300

    Update project
    - Update gradle version
    - Fix gradle issues
    - Update mavenpush.gradle script
    - Update compileSdkVersion
    - Update targetSdkVersion
    - Update buildToolsVersion
    - Migrate to AndroidX

--- sample/src/main/java/com/yalantis/phoenix/sample/BaseRefreshFragment.java
+++ sample/src/main/java/com/yalantis/phoenix/sample/BaseRefreshFragment.java
@@ -1,7 +1,8 @@
 package com.yalantis.phoenix.sample;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+
+import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- sample/src/main/java/com/yalantis/phoenix/sample/ListViewFragment.java
+++ sample/src/main/java/com/yalantis/phoenix/sample/ListViewFragment.java
@@ -2,13 +2,14 @@ package com.yalantis.phoenix.sample;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+
+import androidx.annotation.NonNull;
 
 import com.yalantis.phoenix.PullToRefreshView;
 
